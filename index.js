8f942f2a83c6b1a32767af5a5fa6065d5e3471de
Reggie Barnett <reggiedbarnett@gmail.com>
2015-08-25 16:31:27 -0400

    literally nothing

---
+++ index.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+//passing index.html to server
+app.get('/', function(req, res){
+	res.sendFile(__dirname+'/index.html');
+});
+//listening for connections
+io.on('connection', function(socket){
+	console.log('user connected');
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+});
+
+http.listen(3000, function(){
+	console.log('listening on port 3000');
+});
