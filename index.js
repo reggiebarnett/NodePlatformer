73b9b7a32ed35da02414817c4f93b73ee7c50543
acroul <awc19@pitt.edu>
2015-08-25 18:26:25 -0400

    Reorganize stuff

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 //passing index.html to server
+
+app.use(express.static(__dirname + "/public"));
+
 app.get('/', function(req, res){
-	res.sendFile(__dirname+'/index.html');
+	res.sendFile(__dirname+'/views/index.html');
 });
 //listening for connections
 io.on('connection', function(socket){
