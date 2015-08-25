3efe7f1a523b2b25af44277f051b63575ca2f598
acroul <awc19@pitt.edu>
2015-08-25 19:31:36 -0400

    OHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,5 +1,9 @@
 var canvas;
 var context;
+var map = 
+[[0, 0],
+['x', 'x']];
+
 function setupCanvas() {
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");
@@ -11,4 +15,34 @@ function setupCanvas() {
 	context.strokeRect(0, 0, 800, 600);
 }
 
-setupCanvas();
+function drawMap() {
+	var x = 0;
+	var y = 0;
+	var cell_width = canvas.width / map.length;
+	var cell_height = canvas.height / map[0].length;
+	context.strokeStyle = "#aaaaaa";
+
+	for(var i = 0; i < map.length; i++) {
+		for(var j = 0; j < map[0].length; j++) {
+			if(map[i][j] == 'x') {
+				context.fillStyle = "#666666";
+				context.fillRect(x, y, cell_width, cell_height);
+			} else {
+				context.fillStyle = "#eeeeee";
+				context.fillRect(x, y, cell_width, cell_height);
+			}
+			context.strokeRect(x, y, cell_width, cell_height);
+			x += cell_width;
+		}
+		x = 0;
+		y += cell_height;
+	}
+}
+
+function drawPlayer(player) {
+	context.fillStyle = "#420420";
+	//context.strokeStyle = "#aaaaa";
+
+	context.fillRect(player.x, player.y - player.height, player.width, player.height);
+}
+
